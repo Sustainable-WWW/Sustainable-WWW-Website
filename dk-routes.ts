a2243af7380dbd373217789bfb197c52b126423f
wherecani <michael_andersen93@pm.me>
2025-02-05 14:54:55 +0100

    DK: Switching to more simple routing system for articles

--- dk-routes.ts
+++ dk-routes.ts
@@ -1,49 +1,19 @@
-import express, { Request, Response } from "express";
+import express from "express";
 const router = express.Router();
 
-router.get("/", (_req: Request, res: Response) => {
-    res.render("DK/home");
-});
+router.get("/", (_req, res) => res.render("DK/home"));
+router.get("/om-os", (_req, res) => res.render("DK/om_os"));
+router.get("/kontakt", (_req, res) => res.render("DK/kontakt"));
+router.get("/ledige-stillinger", (_req, res) => res.render("DK/stillinger"));
+router.get("/cookies-og-privatlivspolitik", (_req, res) => res.render("DK/cookie_og_privatlivspolitik"));
 
-router.get("/cases", (_req: Request, res: Response) => {
-    res.render("DK/cases");
-});
+router.get("/cases", (_req, res) => res.render("DK/cases"));
 
-router.get("/om-os", (_req: Request, res: Response) => {
-    res.render("DK/om_os");
-});
+router.get("/artikler", (_req, res) => res.render("DK/artikler"));
 
-router.get("/kontakt", (_req: Request, res: Response) => {
-    res.render("DK/kontakt");
-});
-
-router.get("/ledige-stillinger", (_req: Request, res: Response) => {
-    res.render("DK/stillinger");
-});
-
-router.get("/cookies-og-privatlivspolitik", (_req: Request, res: Response) => {
-    res.render("DK/cookie_og_privatlivspolitik");
-});
-
-router.get("/artikler", (_req: Request, res: Response) => {
-    res.render("DK/artikler");
-});
-
-// BLOG POSTS
-router.get("/artikler/hvad-gor-en-hjemmeside-baeredygtig", (_req: Request, res: Response) => {
-    res.render("DK/Articles/hvad_goer_en_hjemmeside_baeredygtig");
-});
-
-router.get("/artikler/hvorfor-er-baeredygtige-hjemmesider-fremtiden", (_req: Request, res: Response) => {
-    res.render("DK/Articles/hvorfor_er_baeredygtige_hjemmesider_fremtiden");
-});
-
-router.get("/artikler/hvordan-en-baeredygtig-hjemmeside-styrker-dit-brand", (_req: Request, res: Response) => {
-    res.render("DK/Articles/hvordan_en_baeredygtig_hjemmeside_styrker_dit_brand");
-});
-
-router.get("/artikler/er-din-virksomheds-hjemmeside-en-klimasynder", (_req: Request, res: Response) => {
-    res.render("DK/Articles/er_din_virksomheds_hjemmeside_en_klimasynder");
-});
+// Dynamisk routing til blogposts
+router.get("/artikler/:slug", (req, res) => 
+    res.render(`DK/Articles/${req.params.slug.replace(/-/g, "_")}`)
+);
 
 export default router;
