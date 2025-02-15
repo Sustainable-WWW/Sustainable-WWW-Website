fe6348b7c9c32d548c486b24ca4a2b528b961be9
wherecani <michael_andersen93@pm.me>
2025-02-15 21:58:53 +0100

    SE: Renaming routes

--- se-routes.ts
+++ se-routes.ts
@@ -1,15 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-router.get("/", (_req, res) => res.render("SE/home"));
-router.get("/om", (_req, res) => res.render("SE/about"));
-router.get("/kontakt", (_req, res) => res.render("SE/contact"));
-router.get("/lediga-tjanster", (_req, res) => res.render("SE/positions"));
-router.get("/cookies-och-integritetspolicy", (_req, res) => res.render("SE/cookies_and_privacy_policy"));
-router.get("/tjanster", (_req, res) => res.render("SE/services"));
-router.get("/tjanster/webdesign", (_req, res) => res.render("SE/webdesign"));
-router.get("/tjanster/webdevelopment", (_req, res) => res.render("SE/webdevelopment"));
-router.get("/tjanster/analysis-and-optimization", (_req, res) => res.render("SE/analysis_and_optimization"));
+router.get("/", (_req, res) => res.render("SE/hem"));
+router.get("/om", (_req, res) => res.render("SE/om"));
+router.get("/kontakt", (_req, res) => res.render("SE/kontakt"));
+router.get("/lediga-tjanster", (_req, res) => res.render("SE/lediga_tjanster"));
+router.get("/cookies-och-integritetspolicy", (_req, res) => res.render("SE/cookies_och_integritetspolicy"));
+router.get("/tjanster", (_req, res) => res.render("SE/tjanster"));
+router.get("/tjanster/webbdesign", (_req, res) => res.render("SE/webbdesign"));
+router.get("/tjanster/webbutveckling", (_req, res) => res.render("SE/webbutveckling"));
+router.get("/tjanster/analys-och-optimering", (_req, res) => res.render("SE/analys_och_optimering"));
 router.get("/tjanster/hosting", (_req, res) => res.render("SE/hosting"));
 router.get("/sustainable-web-design-in-20-lessons", (_req, res) => res.render("SE/sustainable_web_design_in_20_lessons"));
 
