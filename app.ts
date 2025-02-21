53b8afedf5e73cecdebfd04fcb80a7c1d1df0161
wherecani <michael_andersen93@pm.me>
2025-02-21 07:36:25 +0100

    Global: Rewriting Express.js server to run separate servers for each language

--- app.ts
+++ app.ts
@@ -3,39 +3,52 @@ import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 
-const app = express();
-const port: number = 3036;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+function createLocalizedApp(languageRoutes: express.Router): express.Express {
+    const app = express();
+    
+    // Security middleware
+    app.use(helmet({
+        contentSecurityPolicy: false,
+        hidePoweredBy: true
+    }));
+    app.use(helmet.frameguard({ action: "deny" }));
+    app.use(helmet.xssFilter());
+    
+    // Request parsing
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    
+    // Static assets
+    app.use('/Stylesheets', express.static(path.join(__dirname, 'Public', 'Stylesheets')));
+    app.use('/Scripts', express.static(path.join(__dirname, 'Public', 'JavaScript')));
+    app.use('/Fonts', express.static(path.join(__dirname, 'Public', 'Fonts')));
+    app.use("/Images", express.static(path.join(__dirname, "Media")));
+    
+    // View engine
+    app.set("view engine", "ejs");
+    app.set('views', path.join(__dirname, 'Views'));
+    
+    // Language-specific routes
+    app.use("/", languageRoutes);
+    
+    // Centralized 404 handling
+    app.use((_req, res) => res.status(404).render("404"));
+    
+    return app;
+}
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard());
-app.use(helmet.xssFilter());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Set up the static file serving
-app.use('/Stylesheets', express.static(path.join(__dirname, 'Public', 'Stylesheets')));
-app.use('/Scripts', express.static(path.join(__dirname, 'Public', 'JavaScript')));
-app.use('/Fonts', express.static(path.join(__dirname, 'Public', 'Fonts')));
-// Shows static image files from the image upload folder
-app.use("/Images", express.static(path.join(__dirname, "Media")));
-
-app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'Views'));
-
-// ROUTES
 import en_routes from "./en-routes";
 import se_routes from "./se-routes";
 import dk_routes from "./dk-routes";
 
-app.use("/en", en_routes);
-app.use("/se", se_routes);
-app.use("/dk", dk_routes);
+const enApp = createLocalizedApp(en_routes);
+const seApp = createLocalizedApp(se_routes);
+const dkApp = createLocalizedApp(dk_routes);
 
-// 404 route
-app.use((_req, res) => res.status(404).render("404"));
-
-app.listen(port, () => {
-    console.log("SWWW Website - Running");
-});
+enApp.listen(1001, () => 
+    console.log("English server: http://192.168.50.34:1001"));
+seApp.listen(1002, () => 
+    console.log("Swedish server: http://192.168.50.34:1002"));
+dkApp.listen(1003, () => 
+    console.log("Danish server: http://192.168.50.34:1003"));
